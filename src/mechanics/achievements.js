b3781c90ecb1801c0789eec958f6543c60489a6f
Russia80 <Gimadeev.RF@yandex.ru>
2026-08-09 22:14:09 +0300

    Create achievements.js

---
+++ src/mechanics/achievements.js
@@ -0,0 +1,9 @@
+javascript
+class AchievementSystem {
+  constructor() { this.unlocked = []; this.justUnlocked = []; }
+  check() { return []; }
+  getUnlocked() { return this.unlocked; }
+  getJustUnlocked() { return this.justUnlocked; }
+}
+
+module.exports = { AchievementSystem };
